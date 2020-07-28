6e121383e5df665d7604825c71aa2db55ced1ce7
Guining Pertin <guining.pertin.iitg@gmail.com>
2020-07-28 19:34:32 +0530

    Added calibration script

--- proj1_debug.py
+++ proj1_debug.py
@@ -8,10 +8,14 @@ import cv2
 import cv2.aruco as aruco
 
 def main():
+    #Start camera
     cap = cv2.VideoCapture(1)
     cap.set(cv2.CAP_PROP_BUFFERSIZE, 3)
+    #Set up ArUco
     aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
     params = aruco.DetectorParameters_create()
+    #Set up camera matrix and distortion coeffs
+    # cv_file = cv2.FileStorage("")
     while 1:
         #Detect marker
         _, frame = cap.read()
