bce50dca58ebe2c9894d9759fa0eb17b1eaa0f36
Guining Pertin <guining.pertin.iitg@gmail.com>
2020-07-28 19:55:08 +0530

    Completed ArUco pose estimation

--- proj1_debug.py
+++ proj1_debug.py
@@ -6,6 +6,7 @@ Script to debug detection and motion planning for Project 1
 '''
 import cv2
 import cv2.aruco as aruco
+import numpy as np
 
 def main():
     #Start camera
@@ -15,7 +16,13 @@ def main():
     aruco_dict = aruco.Dictionary_get(aruco.DICT_6X6_250)
     params = aruco.DetectorParameters_create()
     #Set up camera matrix and distortion coeffs
-    # cv_file = cv2.FileStorage("")
+    cv_file = cv2.FileStorage("debug_calib.yaml", cv2.FILE_STORAGE_READ)
+    cam_mtx = cv_file.getNode("camera_matrix").mat()
+    print("Camera Matrix: ")
+    print(cam_mtx)
+    dist_coeff = cv_file.getNode("dist_coeff").mat()[0]
+    print("Distortion Coefficients: ")
+    print(dist_coeff)
     while 1:
         #Detect marker
         _, frame = cap.read()
@@ -27,8 +34,14 @@ def main():
         if np.all(ids != None):
             rvec_list = []
             tvec_list = []
-            for i in range(len(irds)):
-                rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers
+            #Get the estimated pose of the marker wrt camera
+            rvecs, tvecs, _ = aruco.estimatePoseSingleMarkers(corners, 9, cam_mtx, dist_coeff)
+            #Also draw axes
+            for i in range(len(ids)):
+                aruco.drawAxis(detected, cam_mtx, dist_coeff, rvecs[i], tvecs[i], 2)
+            #Debug, print rvecs/tvecs
+            print(tvecs)
+        #Show image
         cv2.imshow("detected", detected)
         k = cv2.waitKey(1)
         if k == 27:
